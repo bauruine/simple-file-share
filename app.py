3387e1d964acc407b1fe2e59e20fe988dc4d0877
Spühler Stefan <git@tuxli.ch>
2020-05-28 20:26:32 +0200

    Change upload path to /upload

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 import os.path
 import random
 import string
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/var/www/upload/files/'
+HOST = 'https://upload.tuxli.ch'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -22,30 +23,33 @@ def generate_filename(secure):
     return random_name
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file:
-            generated_name = generate_filename(request.form.get('secure'))
-            filename = secure_filename(file.filename)
-            extension = os.path.splitext(filename)[1]
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], f'{generated_name}{extension}'))
-            return f'http://127.0.0.1/files/{generated_name}{extension}/{filename}'
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file:
+        generated_name = generate_filename(request.form.get('secure'))
+        filename = secure_filename(file.filename)
+        extension = os.path.splitext(filename)[1]
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f'{generated_name}{extension}'))
+        return f'{HOST}/files/{generated_name}{extension}/{filename}'
+
+
+@app.route('/', methods=['GET'])
+def upload_form():
     return '''
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
+    <form method=post action=/upload enctype=multipart/form-data>
     Secure Filename?  <input type="checkbox" value="true" name="secure"><br>
       <input type=file name=file><br>
       <input type=submit value=Upload>
